847928874d5ad57da055ce1b33d21c75aa00e674
vitaliycatt <ksowy@mail.ru>
2025-07-02 09:30:41 +0300

    chore: code refactoring

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
