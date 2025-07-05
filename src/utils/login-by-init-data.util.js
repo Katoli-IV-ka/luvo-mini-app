835d5b04449fcafae1c5e96bfa5178ccb49bc344
vitaliycatt <ksowy@mail.ru>
2025-07-05 23:13:00 +0300

    feat: add empty state to rating page and change login url

--- src/utils/login-by-init-data.util.js
+++ src/utils/login-by-init-data.util.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "@/constants";
 
 export const loginByInitData = async (initData) => {
-  const response = await axios.post(`${API_URL}/auth/`, {
+  const response = await axios.post(`${API_URL}/auth/login`, {
     init_data: initData,
   });
   return response.data;
