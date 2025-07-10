8c8a1fccc3553a1c1625029540c1886d331b9ae7
vitaliycatt <ksowy@mail.ru>
2025-07-10 14:21:47 +0300

    fix: access token make nullable and code refactiring

--- src/utils/get-auth-tokens.util.js
+++ src/utils/get-auth-tokens.util.js
@@ -4,5 +4,5 @@ export const buildAuthorizationHeader = (token) => `Bearer ${token}`;
 
 export const getAccessToken = () => {
   const { user } = JSON.parse(localStorage.getItem(USER_STORAGE_KEY));
-  return buildAuthorizationHeader(user.accessToken);
+  return user?.accessToken ? buildAuthorizationHeader(user.accessToken) : null;
 };
