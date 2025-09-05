1726c31d7d9b39f21603fba89283f36aabdb7dd3
vitaliycatt <ksowy@mail.ru>
2025-09-05 15:13:03 +0300

    chore: change svg icon

--- src/components/sidebar/swords.jsx
+++ src/components/sidebar/swords.jsx
@@ -6,18 +6,12 @@ export const SwordsIcon = ({ className }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
     className={className}
+    class="lucide lucide-heart-handshake-icon lucide-heart-handshake"
   >
-    <polyline points="14.5 17.5 3 6 3 3 6 3 17.5 14.5" />
-    <line x1="13" x2="19" y1="19" y2="13" />
-    <line x1="16" x2="20" y1="16" y2="20" />
-    <line x1="19" x2="21" y1="21" y2="19" />
-    <polyline points="14.5 6.5 18 3 21 3 21 6 17.5 9.5" />
-    <line x1="5" x2="9" y1="14" y2="18" />
-    <line x1="7" x2="4" y1="17" y2="20" />
-    <line x1="3" x2="5" y1="19" y2="21" />
+    <path d="M19.414 14.414C21 12.828 22 11.5 22 9.5a5.5 5.5 0 0 0-9.591-3.676.6.6 0 0 1-.818.001A5.5 5.5 0 0 0 2 9.5c0 2.3 1.5 4 3 5.5l5.535 5.362a2 2 0 0 0 2.879.052 2.12 2.12 0 0 0-.004-3 2.124 2.124 0 1 0 3-3 2.124 2.124 0 0 0 3.004 0 2 2 0 0 0 0-2.828l-1.881-1.882a2.41 2.41 0 0 0-3.409 0l-1.71 1.71a2 2 0 0 1-2.828 0 2 2 0 0 1 0-2.828l2.823-2.762" />
   </svg>
 );
